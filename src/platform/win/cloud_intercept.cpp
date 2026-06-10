44bb610c19c0d649a16817b843167c9de316924b
Selectively11 <selectively11@users.noreply.github.com>
2026-06-10 14:18:03 -0400

    Add Steam 1781041600 to supported versions

--- src/platform/win/cloud_intercept.cpp
+++ src/platform/win/cloud_intercept.cpp
@@ -3154,7 +3154,7 @@ static void UploadLuaOnShutdown() {
 }
 
 // Supported Steam client versions - patches and RVAs are only valid for these builds. Index 0 is the newest.
-static constexpr uint64_t SUPPORTED_STEAM_VERSIONS[] = { 1780352834ULL, 1779918128ULL, 1779486452ULL, 1778281814ULL };
+static constexpr uint64_t SUPPORTED_STEAM_VERSIONS[] = { 1781041600ULL, 1780352834ULL, 1779918128ULL, 1779486452ULL, 1778281814ULL };
 
 static bool IsSupportedSteamVersion(uint64_t v) {
     for (uint64_t s : SUPPORTED_STEAM_VERSIONS)
